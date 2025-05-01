99fefca903739bc9a570289c88eb91b65163e477
parthergk <gauravkumar81464@gmail.com>
2025-05-01 20:50:26 +0530

    api end points

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,29 @@
 import express, { json, Request, Response } from "express";
 import cors from "cors";
+import { z } from "zod";
 const app = express();
 
 app.use(cors());
 app.use(json());
 
-app.post("/create",(req:Request, res:Response)=>{
+app.post("/groups/create",(req:Request, res:Response)=>{
+
+});
+
+app.post("/groups/join", (req:Request, res:Response)=>{
+
+});
+
+app.post("/groups/share", (req:Request, res:Response)=>{
+
+});
+
+
+app.get("/groups/:id", (req:Request, res:Response)=>{
+
+});
+
+app.post("/user/groups/:userId", (req:Request, res:Response)=>{
 
 })
 
